d8ab9c4e4bcb2f7fb60291c8c07472cac57d3286
KacperStasieluk <61713593+KacperStasieluk@users.noreply.github.com>
2020-04-10 12:37:28 +0200

    Update Newton.py

--- Newton.py
+++ Newton.py
@@ -1,27 +1,36 @@
-tabX = [] tabY = [] 
- 
-ilosc_argumentow = int(input("Wprowadz liczbe argumentow: ")) 
- 
-print() 
- 
-for x in range(ilosc_argumentow):     argument = int(input("Podaj kolejny X: "))     tabX.append(argument) 
- 
-print() 
- 
-for y in range(ilosc_argumentow):     wartosc = int(input("Podaj kolejny Y: "))     tabY.append(wartosc) 
- 
-print() 
- 
-X = int(input("Wprowadz X, dla ktorego obliczasz: ")) 
- 
-print() 
- 
-suma = tabY[0] for i in range(1, ilosc_argumentow):     mnozaca = 1 
- 
-    for j in range(ilosc_argumentow - i):         tabY[j] = (tabY[j+1] - tabY[j]) / (tabX[j+i] - tabX[j]) 
- 
-    for k in range(0,i):             mnozaca *= X - tabX[k] 
- 
-    suma += tabY[0] * mnozaca 
- 
+tabX = []
+tabY = []
+
+ilosc_argumentow = int(input("Wprowadz liczbe argumentow: "))
+
+print()
+
+for x in range(ilosc_argumentow):
+    argument = int(input("Podaj kolejny X: "))
+    tabX.append(argument)
+
+print()
+
+for y in range(ilosc_argumentow):
+    wartosc = int(input("Podaj kolejny Y: "))
+    tabY.append(wartosc)
+
+print()
+
+X = int(input("Wprowadz X, dla ktorego obliczasz: "))
+
+print()
+
+suma = tabY[0]
+for i in range(1, ilosc_argumentow):
+    mnozaca = 1
+
+    for j in range(ilosc_argumentow - i):
+        tabY[j] = (tabY[j+1] - tabY[j]) / (tabX[j+i] - tabX[j])
+
+    for k in range(0,i):
+            mnozaca *= X - tabX[k]
+
+    suma += tabY[0] * mnozaca
+
 print(suma)
