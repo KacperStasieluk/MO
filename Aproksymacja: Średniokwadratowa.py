313c9bcfb2588ee24cfccc0ce125f2497f31dac9
KacperStasieluk <61713593+KacperStasieluk@users.noreply.github.com>
2020-05-25 20:54:48 +0200

    Update Aproksymacja: Średniokwadratowa.py

--- Aproksymacja: Średniokwadratowa.py
+++ Aproksymacja: Średniokwadratowa.py
@@ -1,37 +1,56 @@
 import math
 import numpy as np
+import random
 
-def oblicz_calke(poczatek_p, koniec_p, potega):
-        n = 100
+def calka(ii, jj, poczatek_p, koniec_p):
+    def funkcja(x):
+        return funkcja_bazowa(x, ii) * funkcja_bazowa(x, jj) * funkcja_p(x)
 
-        punkty = [None] * (n+1)
-        punktyti = [None] * (n)
-        punkty[0] = poczatek_p
-        punkty[n] = koniec_p
+    n = 100
 
+    h = (koniec_p-poczatek_p)/n
+
+    punkty = [None] * (n+1)
+    punkty[0] = poczatek_p
+    punkty[n] = koniec_p
+
+    for i in range(1,n):
+        punkty[i] = poczatek_p + (i/n)*(koniec_p-poczatek_p)
+
+    def wynik():
+        wartosc = 0
+        wartosc += funkcja(punkty[0])/2
         for i in range(1,n):
-            punkty[i] = poczatek_p + (i/n)*(koniec_p-poczatek_p)
+            wartosc += funkcja(punkty[i])
+        wartosc += funkcja(punkty[n])/2
+        return h*wartosc
 
-        for i in range(n):
-            punktyti[i] = (punkty[i+1] + punkty[i])/2
+    return(wynik())
 
-        h = (punkty[1]-punkty[0])/2
+def calka2(iii, poczatek_p, koniec_p):
+    def funkcja(x):
+        return funkcja_bazowa(x, iii) * funkcja_f(x) * funkcja_p(x)
 
-        def wynik():
-            wartosc = 0
-            petla1 = 0
-            petla2 = 0
-            wartosc += pow(funkcja_f(punkty[0]), potega)
-            for i in range(0,n):
-                petla1 += pow(funkcja_f(punktyti[i]), potega) 
-            wartosc += 4*petla1
-            for i in range(1,n):
-                petla2 += pow(funkcja_f(punkty[i]), potega)
-            wartosc += 2*petla2
-            wartosc += pow(funkcja_f(punkty[n]), potega)
-            return (h/3)*wartosc
+    n = 100
 
-        return wynik()
+    h = (koniec_p-poczatek_p)/n
+
+    punkty = [None] * (n+1)
+    punkty[0] = poczatek_p
+    punkty[n] = koniec_p
+
+    for i in range(1,n):
+        punkty[i] = poczatek_p + (i/n)*(koniec_p-poczatek_p)
+
+    def wynik():
+        wartosc = 0
+        wartosc += funkcja(punkty[0])/2
+        for i in range(1,n):
+            wartosc += funkcja(punkty[i])
+        wartosc += funkcja(punkty[n])/2
+        return h*wartosc
+
+    return(wynik())
 
 def funkcja_bazowa(x, n):
     return x**n
@@ -42,20 +61,19 @@ def funkcja_p(x):
 def funkcja_f(x):
     return math.sqrt(2 * (x**3) - x + 9)
 
-x = 1
 a = float(input("Podaj dolna wartosc przedzialu: "))
 b = float(input("Podaj gorna wartosc przedzialu: "))
 n = int(input("Podaj stopien wielomianu: "))
 
-tablica = [[None for i in range(n+1)] for j in range(n+1)]
-wyniki = [None for i in range(n+1)]
+tablica = [[0 for i in range(n+1)] for j in range(n+1)]
+wyniki = [0 for i in range(n+1)]
 
 for i in range(n+1):
     for j in range(n+1):
-        tablica[i][j] = funkcja_bazowa(x, i) * funkcja_bazowa(x, j) * funkcja_p(x) * oblicz_calke(a, b, n * (i+j))
+        tablica[i][j] = calka(i, j, a, b) + random.uniform(0.0000000000000001, 0.00000000000001)
 
 for i in range(n+1):
-    wyniki[i] = funkcja_bazowa(x, i) * funkcja_f(x) * funkcja_p(x) * oblicz_calke(a, b, n*i+1)
+    wyniki[i] = calka2(i, a, b) + random.uniform(0.0000000000000001, 0.00000000000001)
 
 print(np.array(tablica))
 print(np.array(wyniki))
