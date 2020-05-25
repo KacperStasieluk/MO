c4743f8e1859a64105974e3aeafbba1ab5ed047f
KacperStasieluk <61713593+KacperStasieluk@users.noreply.github.com>
2020-05-25 19:08:05 +0200

    Update Aproksymacja: Metoda najmniejszych kwadratów.py

--- Aproksymacja: Metoda najmniejszych kwadratów.py
+++ Aproksymacja: Metoda najmniejszych kwadratów.py
@@ -1,7 +1,8 @@
 import math
 import numpy as np
+import random
 
-n = 4
+n = 5
 
 tabX = [-1, -0.5, 0, 0.5, 1]
 tabY = [None for i in range(n)]
@@ -18,19 +19,17 @@ suma = 0
 for k in range(m+1):
     for j in range(m+1):
         for i in range(n):
-            suma += tabX[i] ** (k+j)
-        tablica[k][j] = suma
+            suma = suma + tabX[i] ** (k+j)
+        tablica[k][j] = suma + random.uniform(0.0000000000000001, 0.00000000000001)
         suma = 0
+
 
 suma2 = 0
 for k in range(m+1):
     for i in range(n):
-        suma2 += (tabX[i] ** k) * tabY[i]
-    wyniki[k] = suma2
+        suma2 = suma2 + (tabX[i] ** k) * tabY[i]
+    wyniki[k] = suma2 + random.uniform(0.0000000000000001, 0.00000000000001)
     suma2 = 0
-
-print(np.array(tablica))
-print(np.array(wyniki))
 
 X = float(input("Podaj X: "))
 
