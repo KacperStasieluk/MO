758e72979787af8214a4e36a68785409b330d46c
KacperStasieluk <61713593+KacperStasieluk@users.noreply.github.com>
2020-05-26 20:06:17 +0200

    Update Równania Nieliniowe: Metoda Stycznych.py

--- Równania Nieliniowe: Metoda Stycznych.py
+++ Równania Nieliniowe: Metoda Stycznych.py
@@ -3,7 +3,7 @@ from sympy import *
 
 x = Symbol("x")
 
-y = x ** 2 + x - 5
+y = 3 * (x ** 2) + (20 * x) + 9
 y_prim = y.diff(x)
 y_bis = y_prim.diff(x)
 
@@ -15,19 +15,25 @@ def funkcja(zmienna, poch):
 a = float(input("Podaj początek przedziału: "))
 b = float(input("Podaj koniec przedziału: "))
 e = float(input("Podaj dokładność: "))
+
+iteracje = 1
+
 def styczne():
-    global x0, x1
+    global x0, x1, iteracje
     if(funkcja(a, 0) * funkcja(a, 2) > 0):
         x0 = a
     else: x0 = b
     def petla():
-        global x0, x1
+        global x0, x1, iteracje
         x1 = x0 - (funkcja(x0, 0)/funkcja(x0, 1))
         if(abs(funkcja(x1, 0)) > e or abs(x1 - x0) > e):
             x0 = x1
+            iteracje += 1
             petla()
     petla()
-    print(x1)
+    print("Rozwiązanie: ", x1)
+    print("f(x): ", funkcja(x1, 0))
+    print("Liczba iteracji: ", iteracje)
 
 if((funkcja(a, 1) * funkcja(b, 1) >= 0) and (funkcja(a, 2) * funkcja(b, 2) >= 0)):
     print("Warunki zbieżności zostały spełnione!")
