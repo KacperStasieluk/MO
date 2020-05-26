08683ca037a803ca2c23d65fe9d62e351452d3a6
KacperStasieluk <61713593+KacperStasieluk@users.noreply.github.com>
2020-05-26 20:02:59 +0200

    Update Równania Nieliniowe: Metoda Bisekcji.py

--- Równania Nieliniowe: Metoda Bisekcji.py
+++ Równania Nieliniowe: Metoda Bisekcji.py
@@ -7,10 +7,14 @@ a = float(input("Podaj początek przedziału: "))
 b = float(input("Podaj koniec przedziału: "))
 e = float(input("Podaj dokładność: "))
 
+iteracje = 1
+
 def bisekcja():
-    global a, b, e
+    global a, b, e, iteracje
     if(funkcja((a + b)/2) == 0):
         print("Rozwiązanie: ", (a + b)/2)
+        print("f(x): ", funkcja((a + b)/2))
+        print("Liczba iteracji: ", iteracje)
     elif(funkcja((a + b)/2) * funkcja(a) < 0):
         b = (a + b)/2
     else:
@@ -18,7 +22,10 @@ def bisekcja():
 
     if(abs(funkcja((a + b)/2)) < e):
         print("Rozwiązanie: ", (a + b)/2)
+        print("f(x): ", funkcja((a + b)/2))
+        print("Liczba iteracji: ", iteracje)
     else:
+        iteracje += 1
         bisekcja()
 
 if(funkcja(a) * funkcja(b) < 0): bisekcja()
