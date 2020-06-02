b257304c539f1311f4dd080e1c96cc9555e06be4
KacperStasieluk <61713593+KacperStasieluk@users.noreply.github.com>
2020-06-02 12:16:03 +0200

    Update Rozwiązywanie Układów Równań Liniowych: Metoda Iteracji Prostych.py

--- Rozwiązywanie Układów Równań Liniowych: Metoda Iteracji Prostych.py
+++ Rozwiązywanie Układów Równań Liniowych: Metoda Iteracji Prostych.py
@@ -1,23 +1,29 @@
 import math
 
-wiersze = int(input("Ile równań podasz? "))
-kolumny = int(input("Podaj liczbę współczyników równania: "))
 e = float(input("Podaj dokładność: "))
 
-tab = [[None] * (kolumny + 1)] * wiersze
+tab = [[3,1,2,5],
+       [1,-4,1,-7],
+       [1,2,3,2]]
+
+wiersze = len(tab)
+kolumny = len(tab[0]) - 1
+
 tabX = [0 for i in range(wiersze)]
 kolejnytabX = [0 for i in range(wiersze)]
+dzielniki = [0 for i in range(wiersze)]
 
-for i in range(wiersze ):
-    for j in range(kolumny + 1):
-        if(j < kolumny): tab[i][j] = int(input("Podaj kolejny współczynnik: "))
-        else: tab[i][j] = int(input("Podaj wyraz wolny: "))
+for i in range(wiersze):
+    dzielniki[i] = tab[i][i]
 
 for i in range(wiersze):
     for j in range(kolumny + 1):
-        if(tab[i][i] != 0): tab[i][j] /= tab[i][i]
-        if(i == j): tab[i][j] = 0
-        if(j != kolumny): tab[i][j] *= -1
+        tab[i][j] = tab[i][j] / dzielniki[i]
+    tab[i][i] = 0
+
+for i in range(wiersze):
+    for j in range(kolumny):
+        tab[i][j] *= -1
 
 def petla():
 
@@ -26,8 +32,8 @@ def petla():
 
         nowaWartosc = 0
         for j in range(kolumny + 1):
-            if(i != j and j != kolumny): tab[i][j] *= kolejnytabX[i]
-            nowaWartosc += tab[i][j]
+            if(j != kolumny): nowaWartosc += tab[i][j] * kolejnytabX[j]
+            else: nowaWartosc += tab[i][j]
         kolejnytabX[i] = nowaWartosc
 
         if(abs(kolejnytabX[i] - tabX[i]) < e): warunek -= 1
